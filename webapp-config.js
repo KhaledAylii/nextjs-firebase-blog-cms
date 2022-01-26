e331dc9072801a8fc6c7d4566f9c86ca10a93f06
khaledaylii <khaled.ali@mail.concordia.ca>
2022-01-26 12:18:56 -0800

    add webapp config

---
+++ webapp-config.js
@@ -0,0 +1,21 @@
+// use your branding on the website
+export const config = {
+  metaTitleHome: "YOUR NAME Homepage",
+  metaTitleBlog: "YOUR NAME HERE's Blog",
+  metaDescription: "YOUR NAME's personal home on the web",
+  headerTitle: "your name",
+  homeTabTitle: "home",
+  blogTabTitle: "blog",
+  password: "allowseditingandcreation - CHANGE THIS",
+};
+
+// get these when setting up your firebase project
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
