8b9b267c05ad32324b851cf3433a00b94b58b0b3
khaledaylii <khaled.ali@mail.concordia.ca>
2022-01-26 12:16:04 -0800

    fix progress

--- components/progress/Progress.tsx
+++ components/progress/Progress.tsx
@@ -3,16 +3,16 @@ import { useEffect, useState } from "react";
 export const Progress = () => {
   const [dots, setDots] = useState("");
   useEffect(() => {
-    const int = setInterval(() => {
-      if (dots.length >= 3) {
-        setDots("");
+    const int = setTimeout(() => {
+      if (dots.length >= 4) {
+        setDots(".");
       } else {
         setDots((prevState) => `${prevState}.`);
       }
     }, 500);
     return () => {
-      clearInterval(int);
+      clearTimeout(int);
     };
-  }, []);
+  }, [dots, setDots]);
   return <>{dots}</>;
 };
